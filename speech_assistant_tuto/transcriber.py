08053923dfb57f1dd7d7a6d87b7d479071d6e124
BernardMora <morales.bernardo@uabc.edu.mx>
2024-05-03 09:25:41 -0700

    some changes

--- speech_assistant_tuto/transcriber.py
+++ speech_assistant_tuto/transcriber.py
@@ -1,14 +1,19 @@
-import openai
+import whisper
 
 #Convertir audio en texto
 class Transcriber:
     def __init__(self):
-        pass
-        
+        # Load the whisper model
+        self.model = whisper.load_model("base")
+
     #Siempre guarda y lee del archivo audio.mp3
     #Utiliza whisper en la nube :) puedes cambiarlo por una impl local
     def transcribe(self, audio):
+        # Save the audio file
         audio.save("audio.mp3")
-        audio_file= open("audio.mp3", "rb")
-        transcript = openai.Audio.transcribe("whisper-1", audio_file)
-        return transcript.text
+
+        # Transcribe the audio using whisper
+        result = self.model.transcribe("audio.mp3")
+        print("Resultado: ", result)
+        # Return the transcribed text
+        return result["text"]
