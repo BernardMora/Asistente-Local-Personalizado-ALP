534982673852e5d8a55928abf07845f902fcc10b
BernardMora <morales.bernardo@uabc.edu.mx>
2024-05-03 22:32:13 -0700

    model returns functions

--- speech_assistant_tuto/llm.py
+++ speech_assistant_tuto/llm.py
@@ -1,8 +1,12 @@
 import json
 import os
-from groq import Groq
 import requests
 from tts import TTS
+import ollama
+from langchain_community.llms import Ollama
+from langchain_core.prompts import PromptTemplate
+from langchain.chains import LLMChain
+from langchain.output_parsers import StructuredOutputParser, ResponseSchema
 
 #Clase para utilizar cualquier LLM para procesar un texto
 #Y regresar una funcion a llamar con sus parametros
@@ -11,7 +15,7 @@ from tts import TTS
 
 class LLM():
     def __init__(self):
-        self.endpoint = "http://localhost:11434/api/chat"
+        self.llm = Ollama(model="llama3")
         self.model = "llama3"
 
 # "context": self.context
@@ -22,59 +26,57 @@ class LLM():
         return response.json()
     
     def process_functions(self, text):
-        print("Texto recibido: ", text)
-        # Send the transcribed text to Llama3 with the model name
+        print("Texto: ", text)
+
         messages = [
             {"role": "assistant", "content": "You are a local assistant who will help me achieve the tasks that I ask you to do. This means that you will be able to perform tasks such as sending emails, opening websites, and even controlling my computer. You will be able to do this by calling the functions that I have defined for you. You can ask me to do things like 'send an email', 'open a website', or 'control my computer'. I will provide you with the necessary information to complete these tasks. If I ask you to do something that you are not sure how to do, you can ask me for help. I will be happy to help you with any questions you may have."},
-            {"role": "user", "content": text},
-        ]
-        llama3_response = self.send_to_model(messages)
-        print("Llama3 res", llama3_response)
 
-        if not llama3_response:
+            {"role": "user", "content": "Input text: " + text},
+
+            {"role": "assistant", "content": """Based on the input text, determine the function to be called, any necessary arguments, and a breif description in the form of a message of the set of instructions to be done. Provide your output in the following JSON format:
+            
+            {{
+                "function_name": "...",
+                "arguments": {{ ... }},
+                "message": "..."
+            }}
+            """},
+        ]
+        # Use the LLMChain to process the input text and get the function call
+        response = self.llm.invoke(messages)
+        # Use the LLMChain to process the input text
+        if not response:
             final_response = "No response received from Llama3"
             tts_file = TTS().process(final_response)
             return {"result": "error", "text": final_response, "file": tts_file}
+        print("Response: ", response)
+
+
+        # Find the start and end indices of the JSON object in the string
+        start_index = response.find("{")
+        end_index = response.rfind("}") + 1
+
+        # Extract the JSON object from the string
+        json_str = response[start_index:end_index]
+
+        # Convert the JSON string to a Python dictionary
+        output_dict = json.loads(json_str)
+
+        print("Output dict: ", output_dict)
+
+        # Extract the function name and arguments from the structured output
+        function_name = output_dict["function_name"]
+        arguments = output_dict["arguments"]
+        message = output_dict["message"]
 
         # Do something to interpret the user petition and translate it to a function call
-
-
-        # Function message (optional)
-        function_message = None
-        function_name = None
-
-        # Check for weather function
-        if "tijuana" and ("weather" or "clima") in text.lower():
-            function_message = {
-            "role": "function",
-            "name": "get_weather",
-            "content": json.dumps({"ubicacion": "Tijuana, Baja California"}),
-            }
-            function_name = "get_weather"
-
-        # Check for open_chrome function with search term extraction
-        elif ("browser" or "chrome") in text.lower():
-            search_term = extract_search_term(text)
-            website = f"https://www.google.com/search?q={search_term}"
-            function_message = {
-            "role": "function",
-            "name": "open_chrome",
-            "content": json.dumps({"website": website}),
-            }
-            function_name = "open_chrome"
-
-        # Append the function message if identified
-        if function_message:
-            messages.append(function_message)
-
-        #Nuestro amigo GPT quiere llamar a alguna funcion?
-        if function_message:
-            args = llama3_response.to_dict()['function_call']['arguments'] #Con que datos?
-            print("Funcion a llamar: " + function_name)
-            args = json.loads(args)
-            return function_name, args, llama3_response
         
-        return None, None, llama3_response
+        # Append the function message if identified
+        if output_dict:
+            messages.append(message)
+
+        
+        return function_name, arguments, message
     
     #Una vez que llamamos a la funcion (e.g. obtener clima, encender luz, etc)
     #Podemos llamar a esta funcion con el msj original, la funcion llamada y su
